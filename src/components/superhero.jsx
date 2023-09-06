96417af01bb481dc8d39ad4b5d8711e90849816c
AnnaShp <nyutka_slutskaya@mail.ru>
2023-09-06 17:35:22 +0300

    careate react app with superheroes

---
+++ src/components/superhero.jsx
@@ -0,0 +1,19 @@
+import './superhero.css'
+
+function SuperHero(props) {
+    // const { name, universe, alterego, occupation, friends, superpowers, url, info } = this.props;
+    return (
+        <div className="hero-card">
+            <h2 className='hero-name'> {props.name}</h2>
+            <div className='hero-universe'>Вселенная: {props.universe}</div>
+            <div className='hero-alterego'>Альтер-эго: {props.alterego}</div>
+            <div className='hero-occupation'>Род Деятельности: {props.occupation}</div>
+            <div className='hero-friends'>Друзья: {props.friends}</div>
+            <div className='hero-superpowers'> {props.superpowers}</div>
+            <img src={props.url} />
+            <div className='hero-info'>Суперсилы: {props.info}</div>
+        </div>
+    );
+}
+
+export default SuperHero;
