5b1e7502cb41a40cfd989df1071ca1f58653dcde
AnnaShp <nyutka_slutskaya@mail.ru>
2023-10-05 19:05:35 +0300

    little changes to prettier files

--- src/components/Superhero.jsx
+++ src/components/Superhero.jsx
@@ -1,19 +1,28 @@
-import './Superhero.css'
+import "./Superhero.css";
 
 function SuperHero(props) {
-    // const { name, universe, alterego, occupation, friends, superpowers, url, info } = this.props;
-    return (
-        <div className="hero-card">
-            <h2 className='hero-name'> {props.name}</h2>
-            <div className='hero-universe'>Вселенная: {props.universe}</div>
-            <div className='hero-alterego'>Альтер-эго: {props.alterego}</div>
-            <div className='hero-occupation'>Род Деятельности: {props.occupation}</div>
-            <div className='hero-friends'>Друзья: {props.friends}</div>
-            <div className='hero-superpowers'> {props.superpowers}</div>
-            <img src={props.url} />
-            <div className='hero-info'>Суперсилы: {props.info}</div>
-        </div>
-    );
+  const {
+    name,
+    universe,
+    alterego,
+    occupation,
+    friends,
+    superpowers,
+    url,
+    info,
+  } = props;
+  return (
+    <div className="hero-card">
+      <h2 className="hero-name"> {name}</h2>
+      <div className="hero-universe">Вселенная: {universe}</div>
+      <div className="hero-alterego">Альтер-эго: {alterego}</div>
+      <div className="hero-occupation">Род Деятельности: {occupation}</div>
+      <div className="hero-friends">Друзья: {friends}</div>
+      <div className="hero-superpowers"> {superpowers}</div>
+      <img src={url} />
+      <div className="hero-info">Суперсилы: {info}</div>
+    </div>
+  );
 }
 
 export default SuperHero;
